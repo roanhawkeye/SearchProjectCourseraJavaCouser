8477faeeb586c80948779e8ee7c09d0b6651f566
Daniel Martinez <dmartinez@itexico.net>
2016-12-28 15:24:13 -0600

    Initial commit

---
+++ src/search/example/model/Airport.java
@@ -0,0 +1,31 @@
+package search.example.model;
+
+public class Airport {
+
+	private String city;
+	private String country;
+	private String code3;
+	
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCode3() {
+		return code3;
+	}
+	public void setCode3(String code3) {
+		this.code3 = code3;
+	}
+	
+	
+	
+}
